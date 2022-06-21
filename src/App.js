095c9d226c6c08dfbafb5ceaf80dbc943b1cf894
ljgolding <101936562+ljgolding@users.noreply.github.com>
2022-06-21 07:28:20 -0600

    Fix CSS

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+     
      
         <p>
        <Weather />
         </p>
-      </header>
+ 
     </div>
   );
 }
